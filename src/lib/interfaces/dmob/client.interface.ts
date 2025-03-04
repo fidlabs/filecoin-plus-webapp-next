7a7faca89f1944bcbd5ad5db04e68c6f5a53a6de
Maciej Trojan <maciej.trojan@neti-soft.com>
2025-03-04 10:26:16 +0100

    feat: add DC usage over 90 days column to clients list

--- src/lib/interfaces/dmob/client.interface.ts
+++ src/lib/interfaces/dmob/client.interface.ts
@@ -1,86 +1,98 @@
-import {IAllowanceArray, IApiListCountable, IApiListResponse} from "@/lib/interfaces/dmob/dmob.interface";
+import {
+  IAllowanceArray,
+  IApiListCountable,
+  IApiListResponse,
+} from "@/lib/interfaces/dmob/dmob.interface";
 
-export interface IClientsResponse extends IApiListResponse<IClient>, IApiListCountable {
-  totalRemainingDatacap: string
-  clientsWithActiveDeals: string
-  countOfClientsWhoHaveDcAndDeals: string
-  numberOfClients: string
+export interface IClientsResponse
+  extends IApiListResponse<IClient>,
+    IApiListCountable {
+  totalRemainingDatacap: string;
+  clientsWithActiveDeals: string;
+  countOfClientsWhoHaveDcAndDeals: string;
+  numberOfClients: string;
 }
 
 export interface IClient {
-  id: number
-  addressId: string
-  address: string
-  retries: number
-  auditTrail: string
-  name: string
-  orgName: string
-  initialAllowance: string
-  allowance: string
-  verifierAddressId: string
-  createdAtHeight: number
-  issueCreateTimestamp: string
-  createMessageTimestamp: number
-  verifierName: string
-  dealCount?: number
-  providerCount?: number
-  topProvider?: string
-  receivedDatacapChange: string
-  usedDatacapChange: string
-  allowanceArray: IAllowanceArray[]
-  region: string
-  website: string
-  industry: string
-  remainingDatacap: string
+  id: number;
+  addressId: string;
+  address: string;
+  retries: number;
+  auditTrail: string;
+  name: string;
+  orgName: string;
+  initialAllowance: string;
+  allowance: string;
+  verifierAddressId: string;
+  createdAtHeight: number;
+  issueCreateTimestamp: string;
+  createMessageTimestamp: number;
+  verifierName: string;
+  dealCount?: number;
+  providerCount?: number;
+  topProvider?: string;
+  receivedDatacapChange: string;
+  usedDatacapChange: string;
+  allowanceArray: IAllowanceArray[];
+  region: string;
+  website: string;
+  industry: string;
+  remainingDatacap: string;
+  receivedDatacapChange90Days: string;
+  usedDatacapChange90Days: string;
 }
 
-export interface IClientResponse extends IApiListResponse<IClientDeal>, IApiListCountable{
-  name: string
-  allocatedDatacap: string
-  daAllocatedDatacap: string
-  autoverifierAllocatedDatacap: string
-  efilAllocatedDatacap: string
-  ldnAllocatedDatacap: string
-  remainingDatacap: string
-  addressId: string
+export interface IClientResponse
+  extends IApiListResponse<IClientDeal>,
+    IApiListCountable {
+  name: string;
+  allocatedDatacap: string;
+  daAllocatedDatacap: string;
+  autoverifierAllocatedDatacap: string;
+  efilAllocatedDatacap: string;
+  ldnAllocatedDatacap: string;
+  remainingDatacap: string;
+  addressId: string;
 }
 
 export interface IClientProviderBreakdownResponse {
-  stats: IClientProviderBreakdownStat[]
-  name: string
-  dealCount: string
+  stats: IClientProviderBreakdownStat[];
+  name: string;
+  dealCount: string;
 }
 
 export interface IClientProviderBreakdownStat {
-  provider: string
-  total_deal_size: string
-  percent: string
+  provider: string;
+  total_deal_size: string;
+  percent: string;
 }
 
 export interface IClientDeal {
-  id: number
-  type: string
-  clientId: string
-  providerId: string
-  sectorId: string
-  removed: boolean
-  pieceCid: string
-  pieceSize: string
-  termMax: number
-  prevTermMax: string
-  termMin: number
-  termStart: number
-  expiration: string
-  dealId: number
-  isDDO: boolean
-  isMixedSector: boolean
-  createdAt: string
-  updatedAt: string
+  id: number;
+  type: string;
+  clientId: string;
+  providerId: string;
+  sectorId: string;
+  removed: boolean;
+  pieceCid: string;
+  pieceSize: string;
+  termMax: number;
+  prevTermMax: string;
+  termMin: number;
+  termStart: number;
+  expiration: string;
+  dealId: number;
+  isDDO: boolean;
+  isMixedSector: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
 
-export interface IClientAllocationsResponse extends IApiListResponse<IClient>, IApiListCountable {
-  totalRemainingDatacap: string
-  clientsWithActiveDeals: string
-  countOfClientsWhoHaveDcAndDeals: string
-  numberOfClients: string
+export interface IClientAllocationsResponse
+  extends IApiListResponse<IClient>,
+    IApiListCountable {
+  totalRemainingDatacap: string;
+  clientsWithActiveDeals: string;
+  countOfClientsWhoHaveDcAndDeals: string;
+  numberOfClients: string;
 }
