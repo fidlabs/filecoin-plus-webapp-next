c988a11e1e5194ad40cba865148508f4f7922182
tmaciej-neti <maciej.trojan@neti-soft.com>
2025-03-04 12:39:00 +0100

    fix: correct spelling for IPNI misreporting

--- src/components/charts/compliance/storage-providers/ipni-misreporting.tsx
+++ src/components/charts/compliance/storage-providers/ipni-misreporting.tsx
@@ -1,41 +1,48 @@
-import {useAggregatedIPNIMisreporting} from "@/lib/hooks/cdp.hooks";
-import {ChartWrapper} from "@/app/compliance-data-portal/components/chart-wrapper";
-import {Cell, Pie, PieChart, ResponsiveContainer} from "recharts";
-import {ActiveShape} from "@/components/ui/pie-active-shape";
-import {palette} from "@/lib/utils";
+import { useAggregatedIPNIMisreporting } from "@/lib/hooks/cdp.hooks";
+import { ChartWrapper } from "@/app/compliance-data-portal/components/chart-wrapper";
+import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
+import { ActiveShape } from "@/components/ui/pie-active-shape";
+import { palette } from "@/lib/utils";
 
 interface Props {
   plain?: boolean;
 }
 
-export const IpniMisreporting = ({plain}: Props) => {
+export const IpniMisreporting = ({ plain }: Props) => {
+  const { chartData } = useAggregatedIPNIMisreporting();
 
-  const {
-    chartData
-  } = useAggregatedIPNIMisreporting()
-
-
-  return <ChartWrapper title={"IPNI Missreporting"} id={"IpniMissreporting"} plain={plain}>
-    <ResponsiveContainer width={'100%'} maxHeight={800} aspect={1} debounce={50}>
-      <PieChart>
-        <Pie
-          data={chartData}
-          cx="50%"
-          cy="50%"
-          labelLine={false}
-          outerRadius={'70%'}
-          innerRadius={'55%'}
-          fill="#8884d8"
-          dataKey="value"
-          label={ActiveShape}
-          cursor={'pointer'}
-          paddingAngle={1}
-        >
-          {chartData.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={palette(index)} cursor="pointer" />
-          ))}
-        </Pie>
-      </PieChart>
-    </ResponsiveContainer>
-  </ChartWrapper>
+  return (
+    <ChartWrapper title="IPNI Misreporting" id="IpniMisreporting" plain={plain}>
+      <ResponsiveContainer
+        width={"100%"}
+        maxHeight={800}
+        aspect={1}
+        debounce={50}
+      >
+        <PieChart>
+          <Pie
+            data={chartData}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            outerRadius={"70%"}
+            innerRadius={"55%"}
+            fill="#8884d8"
+            dataKey="value"
+            label={ActiveShape}
+            cursor={"pointer"}
+            paddingAngle={1}
+          >
+            {chartData.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={palette(index)}
+                cursor="pointer"
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </ChartWrapper>
+  );
 };
