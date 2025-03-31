5df68f5a7fc954699277e1f9369897d5185a0f3d
tmaciej-neti <maciej.trojan@neti-soft.com>
2025-03-31 16:22:30 +0200

    feat: provide descriptions for old datacap charts

--- src/app/compliance-data-portal/(pages)/old-datacap/allocated-to-clients/page.tsx
+++ src/app/compliance-data-portal/(pages)/old-datacap/allocated-to-clients/page.tsx
@@ -97,20 +97,30 @@ export default async function OldDatacapAllocatedToClientsPage() {
       id="OldDatacapAllocatedToClients"
       addons={[
         {
+          name: "What's here?",
+          size: 2,
+          value: (
+            <p>
+              A chart showing how much &quot;Old Datacap&quot; was allocated to
+              clients each week, with a per entitity breakdown for each week.
+            </p>
+          ),
+        },
+        {
           name: allocationsTotal.label,
           value: filesize(allocationsTotal.value, { standard: "iec" }),
         },
       ]}
     >
+      <p className="text-sm text-center text-muted-foreground mb-4">
+        Click on the bar to see which entites
+      </p>
       <OldDatacapBreakdownChart
         chartData={chartData}
         drilldown={drilldown}
         drilldownItemLabel="Old Datacap Allocated to Clients: "
         variant="allocator"
       />
-      <p className="text-sm text-center text-muted-foreground">
-        Click on bars to see per week breakdown
-      </p>
     </ChartWrapper>
   );
 }
