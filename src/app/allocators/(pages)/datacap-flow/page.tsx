834b319dffa98d6df58dd2a5a492bf3d3578fe0f
Maciej Trojan <maciej.trojan@neti-soft.com>
2025-04-01 16:36:27 +0200

    feat: replace tree structure chart with datacap flow sankey on allocators page

---
+++ src/app/allocators/(pages)/datacap-flow/page.tsx
@@ -0,0 +1,56 @@
+import { GenericContentHeader } from "@/components/generic-content-view";
+import { JsonLd } from "@/components/json.ld";
+import { Card } from "@/components/ui/card";
+import { generatePageMetadata } from "@/lib/utils";
+import { Metadata } from "next";
+import { Organization, WithContext } from "schema-dts";
+import { allocatorsTabs } from "../../constants";
+import { DCFlowSankey } from "@/app/(dashboard)/components/dc-flow-sankey";
+import { getAllocators, getGoogleSheetAuditSizes } from "@/lib/api";
+
+export const metadata: Metadata = generatePageMetadata({
+  title: "Fil+ DataCap Stats | Datacap Flow",
+  description: "Fil+ DataCap Stats | Datacap Flow",
+  url: "https://datacapstats.io/allocators/datacap-flow",
+});
+
+async function fetchPageData() {
+  const [allocatorsData, sheetData] = await Promise.all([
+    getAllocators({
+      page: "1",
+      showInactive: "true",
+    }),
+    getGoogleSheetAuditSizes(),
+  ]);
+
+  return {
+    allocatorsData,
+    sheetData,
+  };
+}
+
+export default async function DatacapFlowPage() {
+  const { allocatorsData, sheetData } = await fetchPageData();
+  const jsonLdData: WithContext<Organization> = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    name: "Fil+ Datacap Flow",
+    image: "https://cryptologos.cc/logos/filecoin-fil-logo.png?v=040",
+    description: "View allocators datacap flow of Fil+ program",
+  };
+
+  return (
+    <JsonLd data={jsonLdData}>
+      <main className="main-content">
+        <Card className="mt-[50px]">
+          <GenericContentHeader
+            selected={allocatorsTabs[1].value}
+            navigation={allocatorsTabs}
+          />
+
+          <DCFlowSankey allocatorsData={allocatorsData} sheetData={sheetData} />
+        </Card>
+      </main>
+    </JsonLd>
+  );
+}
