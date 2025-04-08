816791dee9a0de85b9476a13523b5ed4f93d28e3
Maciej Trojan <maciej.trojan@neti-soft.com>
2025-04-08 14:42:05 +0200

    feat: add IPNI historical status chart

---
+++ src/app/compliance-data-portal/(pages)/storage-providers/ipni-misreporting-historical/page.tsx
@@ -0,0 +1,33 @@
+import { fetchIPNIMisreportingHistoricalData } from "@/lib/api";
+import {
+  IPNIMisreportingHistoricalChart,
+  IPNIMisreportingHistoricalChartProps,
+} from "./components/ipni-misreporting-historical-chart";
+import { dateToYearWeek } from "@/lib/utils";
+
+type ChartData = IPNIMisreportingHistoricalChartProps["data"];
+
+async function loadChartData(): Promise<ChartData> {
+  const apiData = await fetchIPNIMisreportingHistoricalData();
+
+  const chartData: ChartData = apiData.results.map((item) => {
+    return {
+      name: dateToYearWeek(item.week),
+      notReporting: item.notReporting,
+      notReportingName: "IPNI Not Reporting",
+      misreporting: item.misreporting,
+      misreportingName: "IPNI Misreporting",
+
+      ok: item.ok,
+      okName: "IPNI OK",
+    };
+  });
+
+  return chartData;
+}
+
+export default async function OldDatacapSpentByClientsPage() {
+  const chartData = await loadChartData();
+
+  return <IPNIMisreportingHistoricalChart data={chartData} />;
+}
