6ad9d067b9fd3f4c6a26e71f8004e710e7ba6646
Maciej Trojan <maciej.trojan@neti-soft.com>
2025-06-17 16:26:24 +0200

    feat: add error handling for non existing reports

--- src/app/allocators/(pages)/[id]/(pages)/reports/(pages)/[...report]/page.tsx
+++ src/app/allocators/(pages)/[id]/(pages)/reports/(pages)/[...report]/page.tsx
@@ -1,27 +1,34 @@
-import { getAllocatorReportById } from "@/lib/api";
-import { ICDPAllocatorFullReport } from "@/lib/interfaces/cdp/cdp.interface";
 import { ReportsLayout } from "@/app/allocators/(pages)/[id]/(pages)/reports/(pages)/[...report]/components/reports-layout";
-import { ReportsDetailsProvider } from "@/app/allocators/(pages)/[id]/(pages)/reports/(pages)/[...report]/providers/reports-details.provider";
 import { parseReports } from "@/app/allocators/(pages)/[id]/(pages)/reports/(pages)/[...report]/helpers";
+import { ReportsDetailsProvider } from "@/app/allocators/(pages)/[id]/(pages)/reports/(pages)/[...report]/providers/reports-details.provider";
+import { getAllocatorReportById } from "@/lib/api";
+import { isHTTPError } from "@/lib/http-errors";
+import { notFound } from "next/navigation";
 
-interface IPageProps {
+export const revalidate = 3600;
+
+interface PageProps {
   params: { report: string[]; id: string };
 }
 
-const ReportDetail = async ({ params }: IPageProps) => {
-  const reports = await Promise.allSettled(
-    params.report.map(async (report) =>
-      getAllocatorReportById(params.id, report)
-    )
-  );
-  return (
-    <ReportsDetailsProvider
-      reports={parseReports(
-        reports as PromiseFulfilledResult<ICDPAllocatorFullReport>[]
-      )}
-    >
-      <ReportsLayout />
-    </ReportsDetailsProvider>
-  );
-};
-export default ReportDetail;
+export default async function AllocatorReportDetailPage({ params }: PageProps) {
+  try {
+    const reports = await Promise.all(
+      params.report.map(async (report) =>
+        getAllocatorReportById(params.id, report)
+      )
+    );
+
+    return (
+      <ReportsDetailsProvider reports={parseReports(reports)}>
+        <ReportsLayout />
+      </ReportsDetailsProvider>
+    );
+  } catch (error) {
+    if (isHTTPError(error) && error.status === 404) {
+      return notFound();
+    }
+
+    throw error;
+  }
+}
--- src/app/clients/(pages)/[id]/(pages)/reports/(pages)/[...report]/page.tsx
+++ src/app/clients/(pages)/[id]/(pages)/reports/(pages)/[...report]/page.tsx
@@ -1,25 +1,34 @@
-import { getClientReportById } from "@/lib/api";
-import { IClientFullReport } from "@/lib/interfaces/cdp/cdp.interface";
 import { ReportsLayout } from "@/app/clients/(pages)/[id]/(pages)/reports/(pages)/[...report]/components/reports-layout";
-import { ReportsDetailsProvider } from "@/app/clients/(pages)/[id]/(pages)/reports/(pages)/[...report]/providers/reports-details.provider";
 import { parseReports } from "@/app/clients/(pages)/[id]/(pages)/reports/(pages)/[...report]/helpers";
+import { ReportsDetailsProvider } from "@/app/clients/(pages)/[id]/(pages)/reports/(pages)/[...report]/providers/reports-details.provider";
+import { getClientReportById } from "@/lib/api";
+import { isHTTPError } from "@/lib/http-errors";
+import { notFound } from "next/navigation";
 
-interface IPageProps {
+export const revalidate = 3600;
+
+interface PageProps {
   params: { report: string[]; id: string };
 }
 
-const ReportDetail = async ({ params }: IPageProps) => {
-  const reports = await Promise.allSettled(
-    params.report.map(async (report) => getClientReportById(params.id, report))
-  );
-  return (
-    <ReportsDetailsProvider
-      reports={parseReports(
-        reports as PromiseFulfilledResult<IClientFullReport>[]
-      )}
-    >
-      <ReportsLayout />
-    </ReportsDetailsProvider>
-  );
-};
-export default ReportDetail;
+export default async function ClientReportDetailPage({ params }: PageProps) {
+  try {
+    const reports = await Promise.all(
+      params.report.map(async (report) =>
+        getClientReportById(params.id, report)
+      )
+    );
+
+    return (
+      <ReportsDetailsProvider reports={parseReports(reports)}>
+        <ReportsLayout />
+      </ReportsDetailsProvider>
+    );
+  } catch (error) {
+    if (isHTTPError(error) && error.status === 404) {
+      return notFound();
+    }
+
+    throw error;
+  }
+}
