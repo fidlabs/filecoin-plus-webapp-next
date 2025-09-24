e9f2e3dac893ef42de47cea2be3ec1d9e4f03ae5
Łukasz Wal <143998006+lukasz-wal@users.noreply.github.com>
2025-09-24 10:38:22 +0200

    Add filter for round for the rest of compliance dashboards (#121)

--- src/app/compliance-data-portal/(pages)/storage-providers/compliance/page.tsx
+++ src/app/compliance-data-portal/(pages)/storage-providers/compliance/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ChartWrapper } from "@/app/compliance-data-portal/components/chart-wrapper";
+import { EditionRoundCheckbox } from "@/app/compliance-data-portal/components/edition-round-checkbox";
 import { StackedBarGraph } from "@/app/compliance-data-portal/components/graphs/stacked-bar-graph";
 import { Checkbox } from "@/components/ui/checkbox";
 import { StatsLink } from "@/components/ui/stats-link";
@@ -61,98 +62,101 @@ const StorageProviderCompliance = () => {
   );
 
   return (
-    <ChartWrapper
-      setSelectedScale={setSelectedScale}
-      title="SPs Compliance"
-      selectedScale={selectedScale}
-      dataTabs={dataTabs}
-      currentDataTab={dataTab}
-      setCurrentDataTab={setDataTab}
-      addons={[
-        {
-          name: "What are the metrics",
-          size: 3,
-          value: (
-            <div>
-              <ul>
-                <p className="font-medium text-sm text-muted-foreground">
-                  SP is considered complaint when they:
-                </p>
-                <li className="flex gap-1 items-center">
-                  <Checkbox
-                    checked={retrievabilityMetric}
-                    onCheckedChange={(checked) =>
-                      setRetrievabilityMetric(!!checked)
-                    }
-                  />
-                  <p>
-                    Have retrievability score above average{" "}
-                    {averageSuccessRate
-                      ? `(last week average: ${averageSuccessRate.toFixed(2)}%)`
-                      : ""}
-                    <StatsLink
-                      className="ml-2"
-                      href="/compliance-data-portal/storage-providers/retrievability"
-                    >
-                      Retrievability
-                    </StatsLink>
-                  </p>
-                </li>
-                <li className="flex gap-1 items-center">
-                  <Checkbox
-                    checked={numberOfClientsMetric}
-                    onCheckedChange={(checked) =>
-                      setNumberOfClientsMetric(!!checked)
-                    }
-                  />
-                  <p>
-                    Have at least 3 clients
-                    <StatsLink
-                      className="ml-2"
-                      href="/compliance-data-portal/storage-providers/client-diversity"
-                    >
-                      Client Diversity
-                    </StatsLink>
-                  </p>
-                </li>
-                <li className="flex gap-1 items-center">
-                  <Checkbox
-                    checked={totalDealSizeMetric}
-                    onCheckedChange={(checked) =>
-                      setTotalDealSizeMetric(!!checked)
-                    }
-                  />
-                  <p>
-                    Has at most 30% of the DC coming from a single client
-                    <StatsLink
-                      className="ml-2"
-                      href="/compliance-data-portal/storage-providers/biggest-deals"
-                    >
-                      Biggest Allocation
-                    </StatsLink>
-                  </p>
-                </li>
-              </ul>
-            </div>
-          ),
-        },
-      ]}
-    >
-      <p className="text-sm text-center text-muted-foreground mb-4">
-        Click on a bar to see a list of Service Providers for that time period,
-        matching selected criteria.
-      </p>
-      <StackedBarGraph
+    <>
+      <EditionRoundCheckbox />
+      <ChartWrapper
+        setSelectedScale={setSelectedScale}
+        title="SPs Compliance"
+        selectedScale={selectedScale}
+        dataTabs={dataTabs}
         currentDataTab={dataTab}
-        customPalette={gradientPalette("#4CAF50", "#FF5722", 3)}
-        usePercentage={calcPercentage}
-        data={chartData}
-        scale={scale}
-        isLoading={isLoading}
-        unit={dataTab === "PiB" ? "PiB" : "compliant SP"}
-        onBarClick={handleBarClick}
-      />
-    </ChartWrapper>
+        setCurrentDataTab={setDataTab}
+        addons={[
+          {
+            name: "What are the metrics",
+            size: 3,
+            value: (
+              <div>
+                <ul>
+                  <p className="font-medium text-sm text-muted-foreground">
+                    SP is considered complaint when they:
+                  </p>
+                  <li className="flex gap-1 items-center">
+                    <Checkbox
+                      checked={retrievabilityMetric}
+                      onCheckedChange={(checked) =>
+                        setRetrievabilityMetric(!!checked)
+                      }
+                    />
+                    <p>
+                      Have retrievability score above average{" "}
+                      {averageSuccessRate
+                        ? `(last week average: ${averageSuccessRate.toFixed(2)}%)`
+                        : ""}
+                      <StatsLink
+                        className="ml-2"
+                        href="/compliance-data-portal/storage-providers/retrievability"
+                      >
+                        Retrievability
+                      </StatsLink>
+                    </p>
+                  </li>
+                  <li className="flex gap-1 items-center">
+                    <Checkbox
+                      checked={numberOfClientsMetric}
+                      onCheckedChange={(checked) =>
+                        setNumberOfClientsMetric(!!checked)
+                      }
+                    />
+                    <p>
+                      Have at least 3 clients
+                      <StatsLink
+                        className="ml-2"
+                        href="/compliance-data-portal/storage-providers/client-diversity"
+                      >
+                        Client Diversity
+                      </StatsLink>
+                    </p>
+                  </li>
+                  <li className="flex gap-1 items-center">
+                    <Checkbox
+                      checked={totalDealSizeMetric}
+                      onCheckedChange={(checked) =>
+                        setTotalDealSizeMetric(!!checked)
+                      }
+                    />
+                    <p>
+                      Has at most 30% of the DC coming from a single client
+                      <StatsLink
+                        className="ml-2"
+                        href="/compliance-data-portal/storage-providers/biggest-deals"
+                      >
+                        Biggest Allocation
+                      </StatsLink>
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            ),
+          },
+        ]}
+      >
+        <p className="text-sm text-center text-muted-foreground mb-4">
+          Click on a bar to see a list of Service Providers for that time
+          period, matching selected criteria.
+        </p>
+        <StackedBarGraph
+          currentDataTab={dataTab}
+          customPalette={gradientPalette("#4CAF50", "#FF5722", 3)}
+          usePercentage={calcPercentage}
+          data={chartData}
+          scale={scale}
+          isLoading={isLoading}
+          unit={dataTab === "PiB" ? "PiB" : "compliant SP"}
+          onBarClick={handleBarClick}
+        />
+      </ChartWrapper>
+    </>
   );
 };
 
