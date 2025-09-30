b817f86a14af4b1ae90203e5dc5131c3418812c6
lukas <lukasz.wal@neti-soft.com>
2025-09-30 13:50:14 +0200

    Add information about the sum of ddo and non-ddo piece size

--- src/app/clients/(pages)/[id]/page.tsx
+++ src/app/clients/(pages)/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { LatestClaims } from "@/app/clients/(pages)/[id]/components/latest-claims";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ResponsiveView } from "@/components/ui/responsive-view";
 import { getClientLatestClaimsByClientId } from "@/lib/api";
 import { IApiQuery } from "@/lib/interfaces/api.interface";
+import { convertBytesToIEC } from "@/lib/utils";
 import { Suspense } from "react";
 
 interface IPageProps {
@@ -25,6 +28,28 @@ const ClientDetailsPage = async (pageParams: IPageProps) => {
   return (
     <div className="main-content">
       <Suspense>
+        <div className="md:min-w-fit flex">
+          <ResponsiveView>
+            <div className="grid grid-cols-2 w-full p-4 pb-10 gap-4 md:my-6 md:p-0">
+              <Card>
+                <CardHeader className="p-4">
+                  <CardTitle>Total sum of DDO Piece Size</CardTitle>
+                </CardHeader>
+                <CardContent className="p-4 pt-0">
+                  {convertBytesToIEC(data.totalSumOfDdoPieceSize)}
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className="p-4">
+                  <CardTitle>Total sum of non-DDO Piece Size</CardTitle>
+                </CardHeader>
+                <CardContent className="p-4 pt-0">
+                  {convertBytesToIEC(data.totalSumOfNonDdoPieceSize)}
+                </CardContent>
+              </Card>
+            </div>
+          </ResponsiveView>
+        </div>
         <LatestClaims
           clientId={clientId}
           data={data}
